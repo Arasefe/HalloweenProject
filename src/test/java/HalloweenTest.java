1ceee427f195a95a3857012fa08a6af37bc6d119
Arasefe <ismail_yildirim69@yahoo.com>
2020-10-31 11:01:34 -0500

    Second commit

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -2,5 +2,6 @@ public class HalloweenTest {
     public static void main(String[] args) {
         System.out.println("Halloween test");
         System.out.println("I am about to give everybody a candy for halloween");
+        //This is to see the changes
     }
 }
